a426ab1ad724ad1938d454c55494362953199137
Guilherme Damasio <guilhermedamasiomendes@gmail.com>
2026-05-07 15:46:43 -0300

    Agora, retirei o ExibirMenu da Program.cs e criei uma pasta somente para ele, para melhorar ainda mais a arquitetura do projeto.

--- Program.cs
+++ Program.cs
@@ -10,72 +10,18 @@ namespace ToDoConsole
         {
 
             Console.WriteLine("Bem-vindo(a) ao aplicativo ToDo do Guilherme Damasio!");
-            List<string> listaTarefa = new List<string>();
-            string opcao = "";
+            UI.Menu.ExibirMenu();
+        }
 
-            ExibirMenu();
-
-            while (opcao != "e")
+        class Tarefa
+        {
+            public string Descricao { get; set; }
+            public bool Concluida { get; set; }
+            public Tarefa(string descricao)
             {
-                Console.WriteLine("\nEscolha uma opção:");
-                Console.WriteLine("1 - Adicionar uma tarefa.");
-                Console.WriteLine("2 - Ver todas as tarefas adicionadas.");
-                Console.WriteLine("3 - Remover uma tarefa.");
-                Console.WriteLine("4 - Sair do programa.");
-                opcao = Console.ReadLine();
-                
-                if (opcao == "1")
-                {
-                    Console.WriteLine("Por favor, insira o nome da tarefa para adicionar ao aplicativo.");
-                    string nomeTarefa = Console.ReadLine();
-                    listaTarefa.Add(nomeTarefa);
-                    Console.WriteLine("Tarefa adicionada com sucesso!");
-
-                }
-                else if (opcao == "2")
-                {
-                    Console.WriteLine("Tarefas adicionadas:");
-                    for (int i = 0; i < listaTarefa.Count; i++)
-                    {
-                        Console.WriteLine($"{i + 1}. {listaTarefa[i]}");
-                    }
-                }
-                else if (opcao == "3")
-                {
-                    Console.WriteLine("Digite o número da tarefa que deseja remover:");
-                    int numeroTarefa;
-                    if (int.TryParse(Console.ReadLine(), out numeroTarefa) && numeroTarefa > 0 && numeroTarefa <= listaTarefa.Count)
-                    {
-                        string tarefaRemovida = listaTarefa[numeroTarefa - 1];
-                        listaTarefa.RemoveAt(numeroTarefa - 1);
-                        Console.WriteLine($"Tarefa '{tarefaRemovida}' removida com sucesso!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Número de tarefa inválido. Por favor, tente novamente.");
-                    }
-                }
-                else if (opcao == "4") 
-                {
-                    Console.WriteLine("\nSaindo do programa. Até mais!");
-                    opcao = "e";
-                }
-                else
-                {
-                    Console.WriteLine("Opção inválida. Por favor, escolha uma opção válida.");
-                }
-                
+                Descricao = descricao;
+                Concluida = false;
             }
         }
-        static void ExibirMenu()
-        {
-
-            Console.WriteLine("\nEscolha uma opção:");
-            Console.WriteLine("1 - Adicionar uma tarefa.");
-            Console.WriteLine("2 - Ver todas as tarefas adicionadas.");
-            Console.WriteLine("3 - Remover uma tarefa.");
-            Console.WriteLine("4 - Sair do programa.");
-        }
-
     }
 }
---
+++ UI/ExibirMenu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoConsole.UI
+{
+    public static class Menu
+    {
+        public static void ExibirMenu()
+        {
+
+            Console.WriteLine("\nEscolha uma opção:");
+            Console.WriteLine("1 - Adicionar uma tarefa.");
+            Console.WriteLine("2 - Ver todas as tarefas adicionadas.");
+            Console.WriteLine("3 - Remover uma tarefa.");
+            Console.WriteLine("4 - Sair do programa.");
+        }
+    }
+}
