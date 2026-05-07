41a64a20d525fd1dcfc698f7cff38f0ac37c3689
Guilherme Damasio <guilhermedamasiomendes@gmail.com>
2026-05-07 16:17:04 -0300

    Service atualizada e "ÚTIL".

--- Services/TarefaService.cs
+++ Services/TarefaService.cs
@@ -8,5 +8,20 @@ namespace ToDoConsole.Services
     public class TarefaService
     {
         public List<Tarefa> listaDeTarefas = new List<Tarefa>();
+        public void AdicionarTarefa(Tarefa tarefa)
+        {
+            listaDeTarefas.Add(tarefa);
+        }
+        public void ListarTarefas()
+        {
+            foreach (var tarefa in listaDeTarefas)
+            {
+                Console.WriteLine($"Tarefa: {tarefa.Descricao}");
+            }
+        }
+        public void RemoverTarefa(Tarefa tarefa)
+        {
+            listaDeTarefas.Remove(tarefa);
+        }
     }
 }
